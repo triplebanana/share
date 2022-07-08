82fd411ae211c6ecf8bc81ed9ca67eb2477da4f0
JayChl <arcyze@gmail.com>
2022-07-09 00:26:29 +0900

    Implement banana browser share link UI
    This patch implements page of UI
    for part of handling share link of banana browser.
    If the banana browser share link url is passed,
    the page will display banner of banana browser in shortime

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,23 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ShareLink';
+export class AppComponent implements OnInit {
+  readonly DEFAULT_BANNER_DESCRIPTION: string = "Safer, faster, and more stable"
+  bannerDescription = this.DEFAULT_BANNER_DESCRIPTION
+
+  ngOnInit(): void {
+    // Parse parameters as url and move
+  }
+
+  onBannerClicked() {
+    // If browser is chromium based,
+    // it is allowed to move only when user click action
+  }
 }
