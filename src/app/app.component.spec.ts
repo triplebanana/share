124fa7dc0a758b007d41a5ab6548fe5701a15987
JayChl <arcyze@gmail.com>
2022-07-09 23:36:59 +0900

    Implement banana browser sharelink
    This patch implements scripts for handling
    share link of banana browser.
    
    Because banana browser only support android,
    Running platform is not android,
    move shared url directly using current browser.
    
    If android and banana browser is not installed,
    provide banana browser installation suggestion.
    Once banana browser is installed, move link thorugh banana browser

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -3,15 +3,137 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-import { TestBed } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import { fakeAsync, TestBed, tick } from '@angular/core/testing'
+import { By } from '@angular/platform-browser'
+import { AppComponent } from './app.component'
 
-describe('AppComponent', () => {
+  describe('AppComponent', () => {
+
+  const fakeLocation = {
+    href: ""
+  }
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
-    }).compileComponents();
+      providers: [
+        { provide: 'Location', useValue: fakeLocation }
+      ]
+    }).compileComponents()
   });
-});
+
+  const TRIPLE_BANANA_SHARE_URL = "https://triplebanana.dev/share"
+
+  const USER_AGENT_ANDROID_CHROME_BROWSER =
+    "Mozilla/5.0 (Linux; Android 12; SM-G996N) \
+    AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Mobile Safari/537.36"
+
+  const USER_AGENT_ANDROID_KAKAOTALK_WEB_VIEW =
+    "Mozilla/5.0 (Android; Mobile; rv:13.0) \
+    Gecko/13.0 Firefox/13.0 KAKAOTALK"
+
+  const USER_AGENT_LINUX_CHROME =
+    "Mozilla/5.0 (X11; Linux x86_64) \
+    AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.61 Safari/537.36"
+
+  it('should move android intent including target url when use chrome browser', fakeAsync(() => {
+    // given
+    const shareUrl = "https://google.com"
+    fakeLocation.href = generateShareLink(shareUrl)
+
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+
+
+    spyOnProperty(navigator, 'userAgent').and.returnValue(USER_AGENT_ANDROID_CHROME_BROWSER)
+
+    // when
+    app.ngOnInit()
+
+    // then
+    const calledIntent = fakeLocation.href
+    const splittedIntent = calledIntent.split(';')
+    expect(splittedIntent[0]).toEqual(`intent://${new URL(shareUrl).host}#Intent`)
+    expect(splittedIntent[1]).toEqual(`scheme=https`)
+    expect(splittedIntent[2]).toEqual(`package=${app.PACKAGE_ID}`)
+    expect(splittedIntent[3]).toEqual(`S.browser_fallback_url=${new URL(shareUrl).host}`)
+    expect(splittedIntent[4]).toEqual(`action=android.intent.action.VIEW`)
+    expect(splittedIntent[5]).toEqual(`end`)
+  }));
+
+  it('should move android intent including \
+    target url via iframe when use not chrome browser', fakeAsync(() => {
+    // given
+    const shareUrl = "https://google.com"
+    fakeLocation.href = generateShareLink(shareUrl)
+
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+
+    const navigateWithIframe = spyOn<any>(app, 'navigateWithIframe')
+      .and.callFake((url: string): void => console.log("do not move for test"))
+
+    spyOnProperty(navigator, 'userAgent')
+      .and.returnValue(USER_AGENT_ANDROID_KAKAOTALK_WEB_VIEW)
+
+    // when
+    app.ngOnInit()
+    tick()
+    fixture.detectChanges()
+
+    // then
+    expect(navigateWithIframe).toHaveBeenCalled()
+
+    const description = fixture.debugElement.query(By.css(".bannerDescription"))
+    expect(description.nativeElement.textContent.trim()).toEqual(app.DEFAULT_BANNER_DESCRIPTION)
+
+    // after few time, means failed to move with intent then should move to playstore
+    tick(app.DEFAULT_WAIT_TIME_MS)
+
+    const playStoreUrl = fakeLocation.href
+    expect(playStoreUrl).toEqual(`${app.PLAYSTORE_URL}?id=${app.PACKAGE_ID}`)
+  }));
+
+  it('should just move to target url if not android platform', fakeAsync(() => {
+    // given
+    const shareUrl = "https://google.com"
+    fakeLocation.href = generateShareLink(shareUrl)
+
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+
+    spyOnProperty(navigator, 'userAgent')
+      .and.returnValue(USER_AGENT_LINUX_CHROME)
+
+    // when
+    app.ngOnInit()
+    tick()
+
+    // then
+    const moveUrl = fakeLocation.href
+    expect(moveUrl.toString()).toEqual(shareUrl.toString())
+  }));
+
+  it('should move to banana dev page if invalid sharelink', fakeAsync(() => {
+    // given
+    // shareUrl without "shareLink" param
+    const shareUrl = window.location.toString()
+    fakeLocation.href = shareUrl
+
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+
+    // when
+    app.ngOnInit()
+    tick()
+
+    // then
+    expect(fakeLocation.href).toEqual(app.TRIPLE_BANANA_DEV_URL)
+  }));
+
+  function generateShareLink(url: string) {
+    return `${TRIPLE_BANANA_SHARE_URL}?sharelink=${url}`
+  }
+})
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,8 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core'
+import { ShareLink } from './shareLinkUrl'
 
 @Component({
   selector: 'app-root',
@@ -12,14 +13,107 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   readonly DEFAULT_BANNER_DESCRIPTION: string = "Safer, faster, and more stable"
+  readonly ANDROID_CHROME_CLICK_DESCRIPTION = "Click to open link with Banana browser"
+
+  readonly PACKAGE_ID = "org.triple.banana"
+  readonly DEFAULT_WAIT_TIME_MS = 300
+
+  readonly TRIPLE_BANANA_DEV_URL = "https://triplebanana.dev"
+  readonly PLAYSTORE_URL = "https://play.google.com/store/apps/details"
+
   bannerDescription = this.DEFAULT_BANNER_DESCRIPTION
 
+  private shareLink: ShareLink
+
+  private location: Location
+
+  constructor(
+    @Inject('Location') location: Location
+  ) {
+    this.location = location
+    this.shareLink = this.getShareLinkUrl()
+  }
+
   ngOnInit(): void {
-    // Parse parameters as url and move
+    this.processShareLink()
   }
 
   onBannerClicked() {
-    // If browser is chromium based,
-    // it is allowed to move only when user click action
+    this.gotoLinkOrStore()
+  }
+
+  private processShareLink() {
+    if (!this.shareLink.getUrl()) {
+      console.error("Failed to parse banana link : " + this.location.toString())
+      this.navigate(this.TRIPLE_BANANA_DEV_URL)
+      return
+    }
+
+    this.gotoLinkOrStore()
+  }
+
+  private gotoLinkOrStore() {
+    const userAgent = this.getUserAgent();
+    if (userAgent.match(/Android/)) {
+      if (userAgent.match(/Chrome/)) {
+        this.handleChrome()
+      } else {
+        this.handleOtherBrowsers()
+      }
+    }
+    else {
+      this.navigate(this.shareLink.getUrl())
+    }
+  }
+
+  private getShareLinkUrl(): ShareLink {
+    return new ShareLink(this.location.href)
+  }
+
+  private handleChrome() {
+    this.bannerDescription = this.ANDROID_CHROME_CLICK_DESCRIPTION
+    this.navigate(
+      this.generateAndroidIntent(
+        this.shareLink.getProtocol(),
+        this.shareLink.getUrlWithoutProtocol(),
+        this.PACKAGE_ID)
+    )
+  }
+
+  private handleOtherBrowsers() {
+    setTimeout(() => {
+      this.navigate(this.generatePlayStoreLinkUrl(this.PACKAGE_ID))
+    }, this.DEFAULT_WAIT_TIME_MS)
+
+    this.navigateWithIframe()
+  }
+
+  // traditional method navigate url using iframe
+  private navigateWithIframe(): void {
+    const iframe = document.createElement('iframe')
+    iframe.style.visibility = 'hidden'
+    iframe.src = this.generateAndroidIntent(
+      this.shareLink.getProtocol(),
+      this.shareLink.getUrlWithoutProtocol(),
+      this.PACKAGE_ID
+    )
+    document.body.appendChild(iframe);
+    document.body.removeChild(iframe);
+  }
+
+  private navigate(url: string) {
+    this.location.href = url
+  }
+
+  private getUserAgent(): string {
+    return navigator.userAgent
+  }
+
+  private generateAndroidIntent(scheme: string, url: string, packageId: string): string {
+    return `intent://${url}#Intent;scheme=${scheme};package=${packageId};S.browser_fallback_url=${url};action=android.intent.action.VIEW;end;`
+  }
+
+  private generatePlayStoreLinkUrl(packageId: string) {
+    return `${this.PLAYSTORE_URL}?id=${packageId}`
   }
 }
