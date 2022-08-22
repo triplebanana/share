ee69a148d5d42bf63ec78cf20b1d362462d1b2c5
JayChl <arcyze@gmail.com>
2022-08-22 23:22:53 +0900

    Implement ShareLinkUrl class
    This patch implements ShareLinkUrl class to parse
    input share link url to destination share url and protocol

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,8 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core'
+import { ShareLink } from './shareUrl/sharelink-url'
 
 @Component({
   selector: 'app-root',
@@ -11,15 +12,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+  readonly TRIPLE_BANANA_DEV_URL = "https://triplebanana.dev"
+
   readonly DEFAULT_BANNER_DESCRIPTION: string = "Safer, faster, and more stable"
+  readonly ANDROID_CHROME_CLICK_DESCRIPTION = "Click to open link with Banana browser"
+
+  readonly CLICK_GUIDE_WAITING_IN_MS = 500
+
   bannerDescription = this.DEFAULT_BANNER_DESCRIPTION
 
-  ngOnInit(): void {
-    // Parse parameters as url and move
+  private shareLink: ShareLink
+
+  constructor(
+    @Inject('Location') private location: Location
+  ) {
+    this.shareLink = this.getShareLinkUrl()
   }
 
-  onBannerClicked() {
-    // If browser is chromium based,
-    // it is allowed to move only when user click action
+  ngOnInit(): void {
+    this.processShareLink()
+  }
+
+  navigate(): void {
+    // Not Implemented
+  }
+
+  private processShareLink(): void {
+    if (!this.shareLink.getUrl()) {
+      console.error("Failed to parse banana link : "
+        + this.location.href.toString())
+      this.location.href = this.TRIPLE_BANANA_DEV_URL
+      return
+    }
+
+    this.navigate()
+
+    setTimeout(() => {
+      this.bannerDescription = this.ANDROID_CHROME_CLICK_DESCRIPTION
+    }, this.CLICK_GUIDE_WAITING_IN_MS)
+  }
+
+  private getShareLinkUrl(): ShareLink {
+    return new ShareLink(this.location.href)
   }
 }
---
+++ src/app/shareUrl/sharelink-url.spec.ts
@@ -0,0 +1,35 @@
+// Copyright 2022 The Triple Banana Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+import { ShareLink } from './sharelink-url'
+
+describe('ShareLink', () => {
+  it('should parse share link', () => {
+    const bananaShareLink = "https://triplebanana.dev?sharelink=https://www.google.co.kr/imghp?hl=ko&ogbl"
+    const shareLink = new ShareLink(bananaShareLink)
+
+    expect(shareLink.getUrl()).toEqual("https://www.google.co.kr/imghp?hl=ko&ogbl")
+    expect(shareLink.getProtocol()).toEqual("https")
+    expect(shareLink.getUrlWithoutProtocol()).toEqual("www.google.co.kr/imghp?hl=ko&ogbl")
+  })
+
+  it('should return empty url if not contain sharelink url', () => {
+    const notContainedShareUrl = "https://www.google.co.kr/imghp?hl=ko&ogbl"
+    const shareLink = new ShareLink(notContainedShareUrl)
+
+    expect(shareLink.getUrl()).toEqual("")
+    expect(shareLink.getProtocol()).toEqual("")
+    expect(shareLink.getUrlWithoutProtocol()).toEqual("")
+  })
+
+  it('should return proper scheme', () => {
+    const notContainedShareUrl = "https://triplebanana.dev?sharelink=http://www.google.co.kr"
+    const shareLink = new ShareLink(notContainedShareUrl)
+
+    expect(shareLink.getUrl()).toEqual("http://www.google.co.kr")
+    expect(shareLink.getProtocol()).toEqual("http")
+    expect(shareLink.getUrlWithoutProtocol()).toEqual("www.google.co.kr")
+  })
+})
